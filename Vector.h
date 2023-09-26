794c438b1613569260e647f6757678866234a533
RediiEBR <josemanuelmartin.fuentezuelas@gmail.com>
2023-09-26 10:23:39 +0200

    pasoAPortatilGrande

---
+++ Vector.h
@@ -0,0 +1,12 @@
+#ifndef IMAGENES_VDINAMICO_H
+#define IMAGENES_VDINAMICO_H
+template<typename T>
+class Vector
+{
+private:
+    int lsize,tsize;//as que portion that has been used of the vector, and the total size
+public:
+
+};
+
+#endif //IMAGENES_VDINAMICO_H
--- forwardList.h
+++ forwardList.h
@@ -1,3 +1,7 @@
+/**
+ * @brief definition for the forwardList, linked list with the next pointer as a way of iterate through the list
+ * @author josemanuelmartin.fuentezuelas@gmail.com
+*/
 template<typename T>
 class forwardList
 {
@@ -12,29 +16,69 @@ private:
     public:
         X value;
         Node * next;
+        /**
+         * @brief as the default node constructor
+         * the value is set as the default X datatype constructor and the next value is set to nullptr
+        */
         Node () : value() , next( nullptr ){};
         //https://www.scaler.com/topics/cpp-explicit/
-        explicit Node ( X & val ) : value ( val ) , next ( nullptr ) { } ;
+        /**
+         * @brief given X as a value, the node will create with not a next Node to point at
+        */
+        explicit Node ( X & val ) : value ( val ) , next ( nullptr ) {} ;
     };
     Node < T > * head , tail ;
     unsigned int dimension = 0 ;
 public:
-
+    /**
+     * @brief class Iterator
+    */
     class Iterator
     {
+    private:
         Node < T > * node ;
     public:
-        Iterator ( ) : node( nullptr ) { } ;
+        /**
+         * @brief default iterator constructor
+        */
+        Iterator (  ) : node( nullptr ) { } ;
+        /**
+         * @brief Node constructor given a node
+        */
         explicit Iterator( Node < T > * p ) : node ( p ) { } ;
+        /**
+         * @brief nextOne function that makes the iterator go forward once
+        */
         void nextOne();
+        /**
+         * @brief advance function that makes the iterator go forward a given number of times.
+        */
         void advance(unsigned int howMany);
 
     };
-    forwardList () : head (  ) , tail(  ) , dimension(0) { } ;
+    /**
+     * @brief default forwardList constructor
+    */
+    forwardList () : head ( nullptr ) , tail( nullptr ) , dimension(0) { } ;
+    /**
+     * @brief copy constructor created to the forwardList
+    */
     forwardList ( const forwardList < T >  & origin) ;
+    /**
+     * @brief assignment operator overwrite for this container
+    */
     forwardList < T >  & operator = ( const forwardList < T > & origin ) ;
+    /**
+     * @brief plus operator made for this container
+    */
     forwardList < T >  operator + ( const forwardList < T > & origin ) ;
+    /**
+     * @return the first element of this list
+    */
     T & begin () { return this -> head -> value ; } ;
+    /**
+     * @return the last element of the list
+    */
     T & end () { return this -> tail -> value ; } ;
     void push_front ( T & data );
     void push_back ( T & data );
@@ -47,6 +91,47 @@ public:
     Iterator iterador();
 };
 
+template<typename T>
+forwardList<T>::forwardList(const forwardList<T> & origin)
+{
+    if(origin.size()==0)//lista de entrada vacía
+    {
+        this->head = this->tail = nullptr ;
+        this->dimension = 0 ;
+        return;
+    }
+    //else
+    Node<T>*p = origin.head;
+    Node<T>*q = this->head;
+    Node<T>*a = this->head;
+    while (p)
+    {
+
+        q = new Node<T>(p->value);
+        if(q!=head)
+        {
+            a->next=q;
+            a = q;
+        }
+        q=q->next;
+        p=p->next;
+    }
+}
+template<typename T>
+void forwardList<T>::push_back(T & data)
+{
+    Node<T>*newOne(data);//created the new node
+    this->tail->next=newOne;//Next one to tail is new tail
+    this->tail=newOne;//tail is the new element, but it's next one pointer is aiming to itself
+    this->tail->next = nullptr;//by definition, a forwardList's tail element has no pointer to next element, so be it
+}
+template<typename T>
+void forwardList<T>::push_front(T&data)
+{
+    Node<T>*newOne(data);//create the new node
+    newOne->next=this->head;//next node of the new node is the current head
+    this->head=newOne;//head is now the new node
+}
 template<typename T>
 forwardList<T> forwardList<T>::operator+(const forwardList<T> &origin) {
     forwardList<T> ret;
@@ -93,32 +178,7 @@ void forwardList<T>::Iterator::nextOne()
     this->node=node->next;
 }
 
-template<typename T>
-forwardList<T>::forwardList(const forwardList<T> & origin)
-{
-    if(origin.size()==0)//lista de entrada vacía
-    {
-        this->head = this->tail = nullptr ;
-        this->dimension = 0 ;
-        return;
-    }
-    //else
-    Node<T>*p = origin.head;
-    Node<T>*q = this->head;
-    Node<T>*a = this->head;
-    while (p)
-    {
 
-        q = new Node<T>(p->value);
-        if(q!=head)
-        {
-            a->next=q;
-            a = q;
-        }
-        q=q->next;
-        p=p->next;
-    }
-}
 
 template<typename T>
 forwardList<T>::Iterator forwardList<T>::iterador()
